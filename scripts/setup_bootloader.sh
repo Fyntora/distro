8d15025ab217d5de5b111247110e4b3c008191b4
kurboos <kurboos@outlook.be>
2025-12-28 18:15:55 +0100

    Debug

--- scripts/setup_bootloader.sh
+++ scripts/setup_bootloader.sh
@@ -12,6 +12,7 @@ mkdir -p "$ROOTFS_DIR"/boot/grub
 
 # Copy grub images from host
 cp -r /usr/lib/grub/i386-pc "$ROOTFS_DIR"/boot/grub/
+ls "$ROOTFS_DIR"/boot/grub/i386-pc/
 
 # Write grub.cfg
 cat > "$ROOTFS_DIR"/boot/grub/grub.cfg << EOF
