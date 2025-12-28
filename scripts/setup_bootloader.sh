e0372dd74eed468d4a1a7a14e9d689f361208116
kurboos <kurboos@outlook.be>
2025-12-28 19:11:18 +0100

    fix

--- scripts/setup_bootloader.sh
+++ scripts/setup_bootloader.sh
@@ -17,9 +17,6 @@ cat > "$ROOTFS_DIR"/boot/grub/grub.cfg << EOF
 set default=0
 set timeout=5
 
-insmod iso9660
-set root=(cd)
-
 menuentry "Fyntora Linux" {
     linux /boot/vmlinuz-6.6.0 root=/dev/sr0
     initrd /boot/initrd.img
